50f8564ca3b6b343222dcf20209a9b18b6201c9e
LeO_GuRdU <leogurdulich@gmail.com>
2022-01-15 11:52:14 -0300

    Componentes y Rutas Agregadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { PortafolioComponent } from './componentes/portafolio/portafolio.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'educacion', component: EducacionComponent},
+  {path: 'experiencia', component: ExperienciaComponent},
+  {path: 'portafolio', component: PortafolioComponent},
+  {path: 'contacto', component: ContactoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
+import { alarm, alarmFill, alignBottom } from 'ngx-bootstrap-icons';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './componentes/sidebar/sidebar.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { PortafolioComponent } from './componentes/portafolio/portafolio.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+
+
+const icons = {
+  alarm,
+  alarmFill,
+  alignBottom
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    PortafolioComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxBootstrapIconsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
